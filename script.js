88dc1fda9883c0347012dd00bd9c390e3fbf3fe4
freecoder21 <freecoder21@gmail.com>
2025-03-04 04:09:16 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,43 +2,52 @@ const container = document.querySelector(".container")
 const predict_btn = document.getElementById('Predictions')
 
 // PREDICTION BUTTON
-
+let count = 0;
 predict_btn.addEventListener('click',()=>{
-  const existing_boards = document.querySelectorAll(".game_board")
-  
-  if (existing_boards.length === 0) {
-    console.error("No game board found!");
+  if(count == 6){
     return;
-}
-  const firstBoard = container.firstElementChild
-  const circles = firstBoard.querySelectorAll(".circles")
-
-  if (circles.length === 0) {
-    console.error("No circles found in the last game board!");
-    return;
-}
-  predict_btn.disabled = true; // Disable the button to prevent multiple clicks
-  predict_btn.textContent = "Loading... ⏳"; // Change button text
+  }else{
+    count+=1;
+    const existing_boards = document.querySelectorAll(".game_board")
+    
+    if (existing_boards.length === 0) {
+      console.error("No game board found!");
+      return;
+  }
+    const firstBoard = container.firstElementChild
+    const circles = firstBoard.querySelectorAll(".circles")
   
-  setTimeout(()=>{
-    const randomIndex = Math.floor(Math.random()*circles.length)
-
-  circles.forEach(circle=>{
-    circle.style.backgroundImage = ''
-  });
-  const appleImage = 'images/apple3.jpeg'
-  circles[randomIndex].style.backgroundImage = `url(${appleImage})`
-   predict_btn.disabled = false
-   predict_btn.textContent = 'Prediction 🍏'
-
-  createNewBoard()
-  },1000)
+    if (circles.length === 0) {
+      console.error("No circles found in the last game board!");
+      return;
+  }
+    predict_btn.disabled = true; // Disable the button to prevent multiple clicks
+    predict_btn.textContent = "Loading... ⏳"; // Change button text
+    
+    setTimeout(()=>{
+      const randomIndex = Math.floor(Math.random()*circles.length)
+  
+    circles.forEach(circle=>{
+      circle.style.backgroundImage = ''
+    });
+    const appleImage = 'images/apple3.jpeg'
+    circles[randomIndex].style.backgroundImage = `url(${appleImage})`
+     predict_btn.disabled = false
+     predict_btn.textContent = 'Prediction 🍏'
+  
+    createNewBoard()
+    },1000)
+  }
+ 
 
 })
 
  // FUNCTION TO CREATE A NEW BOARD
 
 function createNewBoard(){
+  if(count==6){
+    return;
+  }else{
     const newContainer = document.createElement('div')
     newContainer.className  = 'game_board';
     for(let i=0; i<5; i++){
@@ -48,6 +57,7 @@ function createNewBoard(){
     }
 
     container.insertBefore(newContainer, container.firstChild)
+}
 }
 // CLEARING FUNCTION 
 
@@ -63,4 +73,5 @@ function clearGameBoard(){
 
     container.insertBefore(newContainer, container.firstChild)
 
+  
 }
